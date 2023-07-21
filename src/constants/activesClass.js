c66cff817204735b7db33bd04d71f4bfc1a46f8a
54Yunyun <102648992+54Yunyun@users.noreply.github.com>
2023-07-21 14:03:53 +0800

    修改圖片路徑

--- src/constants/activesClass.js
+++ src/constants/activesClass.js
@@ -2,32 +2,32 @@
 const activesClass = [
   {
     name: '節慶活動',
-    imgUrl: '../assets/img/active_1.jpeg',
+    imgUrl: '/assets/img/active_1.jpeg',
     value: '節慶活動',
   },
   {
     name: '自行車活動',
-    imgUrl: '../assets/img/active_2.jpeg',
+    imgUrl: '/assets/img/active_2.jpeg',
     value: '自行車活動',
   },
   {
     name: '遊憩活動',
-    imgUrl: '../assets/img/active_3.jpeg',
+    imgUrl: '/assets/img/active_3.jpeg',
     value: '遊憩活動',
   },
   {
     name: '產業文化',
-    imgUrl: '../assets/img/active_4.jpeg',
+    imgUrl: '/assets/img/active_4.jpeg',
     value: '產業文化活動',
   },
   {
     name: '年度活動',
-    imgUrl: '../assets/img/active_5.jpeg',
+    imgUrl: '/assets/img/active_5.jpeg',
     value: '年度活動',
   },
   {
     name: '四季活動',
-    imgUrl: '../assets/img/active_6.jpeg',
+    imgUrl: '/assets/img/active_6.jpeg',
     value: '四季活動',
   },
 ];
--- src/constants/scenicSpotClass.js
+++ src/constants/scenicSpotClass.js
@@ -1,37 +1,37 @@
 const scenicSpotClass = [
     {
       name: '自然風景類',
-      imgUrl: '../assets/img/place_1.jpeg',
+      imgUrl: '/assets/img/place_1.jpeg',
       value: '自然風景類',
     },
     {
       name: '觀光工廠類',
-      imgUrl: '../assets/img/place_2.jpeg',
+      imgUrl: '/assets/img/place_2.jpeg',
       value: '觀光工廠類',
     },
     {
       name: '遊憩類',
-      imgUrl: '../assets/img/place_3.jpeg',
+      imgUrl: '/assets/img/place_3.jpeg',
       value: '遊憩類',
     },
     {
       name: '休閒農業類',
-      imgUrl: '../assets/img/place_4.jpeg',
+      imgUrl: '/assets/img/place_4.jpeg',
       value: '休閒農業類',
     },
     {
       name: '生態類',
-      imgUrl: '../assets/img/place_5.jpeg',
+      imgUrl: '/assets/img/place_5.jpeg',
       value: '生態類',
     },
     {
       name: '溫泉類',
-      imgUrl: '../assets/img/place_6.jpeg',
+      imgUrl: '/assets/img/place_6.jpeg',
       value: '古蹟類',
     },
     {
       name: '古蹟類',
-      imgUrl: '../assets/img/place_7.jpeg',
+      imgUrl: '/assets/img/place_7.jpeg',
       value: '古蹟類',
     },
   ];
