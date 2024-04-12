f3aef825ca21655112515a25423cb7c5731fd6e6
yuna_wang <102648992+54Yunyun@users.noreply.github.com>
2024-04-12 14:11:06 +0800

    測試SEO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import prerender from 'vite-plugin-prerender';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    prerender({
+      // 指定要預渲染的路徑
+      pages: ['/'],
+    })],
 })
