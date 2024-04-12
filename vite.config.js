3d4c4e1e8c7a6350db01e5cf32c33723f1b1049b
yuna_wang <102648992+54Yunyun@users.noreply.github.com>
2024-04-12 14:29:34 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// 使用 import 代替 require
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import prerender from 'vite-plugin-prerender';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     prerender({
-      // 指定要預渲染的路徑
       pages: ['/'],
-    })],
-})
+    })
+  ]
+});
